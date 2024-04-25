60e7fe13679daf1b4feb5816a7e9bdc620045d4b
Kibreab Aklilu <Akkeba37@gmail.com>
2024-04-25 21:40:09 +0300

    Setup the mongodb on project

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { connectDB } from "./config/database.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+connectDB();
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Server listening on port ${port} `));
